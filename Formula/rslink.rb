15884f87a68655baeae9771f35442216f1972e76
Yogi <yodeput@gmail.com>
2026-02-02 12:48:01 +0700

    Fix SHA256 checksum for rslink v1.0.0

--- Formula/rslink.rb
+++ Formula/rslink.rb
@@ -2,7 +2,7 @@ class Rslink < Formula
   desc "RSync + Link - Copy/move folders with rsync and create symlinks"
   homepage "https://github.com/yodeput/rslink"
   url "https://github.com/yodeput/rslink/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 :no_check
+  sha256 "d2f534549ce413de014795caa4f955c5102c2edc3baac580e1d5f904158ce624"
 
   def install
     bin.install "rslink"
